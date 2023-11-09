59fb9d7c15a324c42118429883ca3cddfe6d2cf8
Kevin <qeven03@gmail.com>
2023-11-09 13:24:02 -0600

    touche

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    rollupOptions: {
-      external: [
-        "/home/kevineitor/repos/shopping-cart/src/components/Home/Home.css",
-        "/home/kevineitor/repos/shopping-cart/src/App.css",
-        '/home/kevineitor/repos/shopping-cart/src/components/Shop/Shop.css',
-        '/home/kevineitor/repos/shopping-cart/src/components/Cart/Cart.css',
-        '/home/kevineitor/repos/shopping-cart/src/components/ProductInfo/ProductInfo.css',
-        '/home/kevineitor/repos/shopping-cart/src/App.jsx',
-        "/home/kevineitor/repos/shopping-cart/src/components/Home/Home.jsx",
-        '/home/kevineitor/repos/shopping-cart/src/components/Shop/Shop.jsx',
-        '/home/kevineitor/repos/shopping-cart/src/components/Cart/Cart.jsx',
-        '/home/kevineitor/repos/shopping-cart/src/components/ProductInfo/ProductInfo.jsx',
-        '/react',
-        '/react-dom/client',
-        "/react-router-dom",
-        '/react-transition-group',
-        '/home/kevineitor/repos/shopping-cart/src/main.jsx'
-      ],
-    },
-  },
 })
