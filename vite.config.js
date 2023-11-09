5997b55741f7de2c2a376e71034297b02e11a095
Kevin <qeven03@gmail.com>
2023-11-09 12:56:14 -0600

    touche

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,13 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["/home/kevineitor/repos/shopping-cart/src/components/Home/Home.css"],
+      external: [
+        "/home/kevineitor/repos/shopping-cart/src/components/Home/Home.css",
+        "/home/kevineitor/repos/shopping-cart/src/App.css",
+        '/home/kevineitor/repos/shopping-cart/src/components/Shop/Shop.css',
+        '/home/kevineitor/repos/shopping-cart/src/components/Cart/Cart.css',
+        '/home/kevineitor/repos/shopping-cart/src/components/ProductInfo/ProductInfo.css'
+      ],
     },
   },
 })
